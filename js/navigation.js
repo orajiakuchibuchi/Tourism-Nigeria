1cbf7e5dfbfd85116489eb937cbaf74761639303
Home <home@Brians-MacBook-Air.local>
2021-04-21 09:54:12 +0100

    deployment

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,4 @@
 function navigateToState(page){
-    window.location.href = "http://localhost:8888/buchis%20work/listing/lagos.html";
+    // window.location.href = "http://localhost:8888/buchis%20work/listing/lagos.html";
+    window.location.href = "https://orajiakuchibuchi.github.io/Tourism-Nigeria/lagos.html";
 }
